81d219faee2157e0e523941f64a676bc7cbe6a21
Jeffrey Lubin <Jeffy.lubin@gmail.com>
2019-11-11 14:37:45 -0600

    fixed php form email bugs

--- form_to_email.php
+++ form_to_email.php
@@ -30,22 +30,6 @@
         echo "Invalid Email";
         exit;
     }
-
-    if(isset($_POST["submit"])){
-        // Checking For Blank Fields..
-        if($_POST["name"]==""||$_POST["email"]==""||$_POST["msg"]==""){
-        echo "Fill All Fields..";
-        }else{
-        // Check if the "Sender's Email" input field is filled out
-        $email=$_POST['email'];
-        // Sanitize E-mail Address
-        $email =filter_var($email, FILTER_SANITIZE_EMAIL);
-        // Validate E-mail Address
-        $email= filter_var($email, FILTER_VALIDATE_EMAIL);
-        if (!$email){
-        echo "Invalid Sender's Email";
-        }
-    else{
     
     $name = $_POST['name'];
     $email = $_POST['email'];
