92c3bf331040c0ae71d188e70e9915a8e6b97fb9
Jeffrey Lubin <Jeffy.lubin@gmail.com>
2019-11-11 14:36:09 -0600

    Added php form to email function

---
+++ form_to_email.php
@@ -0,0 +1,64 @@
+<?php
+
+    function isInjected($str){
+        $injections = 
+        array(
+        '(\n+)',
+        '(\r+)',
+        '(\t+)',
+        '(%0A+)',
+        '(%0D+)',
+        '(%08+)',
+        '(%09+)'
+        );
+
+        $inject = join('|', $injections);
+        $inject = "/$inject/i";
+
+        if(preg_match($inject,$str))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    if(isInjected($visitor_email)){
+        echo "Invalid Email";
+        exit;
+    }
+
+    if(isset($_POST["submit"])){
+        // Checking For Blank Fields..
+        if($_POST["name"]==""||$_POST["email"]==""||$_POST["msg"]==""){
+        echo "Fill All Fields..";
+        }else{
+        // Check if the "Sender's Email" input field is filled out
+        $email=$_POST['email'];
+        // Sanitize E-mail Address
+        $email =filter_var($email, FILTER_SANITIZE_EMAIL);
+        // Validate E-mail Address
+        $email= filter_var($email, FILTER_VALIDATE_EMAIL);
+        if (!$email){
+        echo "Invalid Sender's Email";
+        }
+    else{
+    
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone_number = $_POST['phone_number'];
+    $message = $_POST['message'];
+
+    $to="jeffy.lubin@gmail.com";
+
+    $email_from = $email;
+    $email_subject = "You have a new message from ".$name."!";
+    $email_message =  $name." is reaching out! This is what he said,\n".$message."\n They also provided their phone number if you'd want to give them a call!\n".$phone_number;
+
+    mail($to, $email_subhect, $email_message);
+    echo "Your email has been sent succesfully! You should hear back from me in about 24-48 hours, thank you!";
+    }
+?>
