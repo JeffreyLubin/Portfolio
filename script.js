4d76253ec4a56953ba64e78fe04ed5fcbc2eb2d6
Jeffrey Lubin <jeffy.lubin@gmail.com>
2019-11-15 04:30:38 -0600

    Added JS functionaltiy to music projects page.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var slider_content=document.getElementById('box');
 var image = ["a","b","e","h"];
 let g_s = document.getElementById('music_logo_container');
+let studio = document.getElementById('music_logo_container');
 console.log(g_s);
 
 /*CREATING THE NAV ICON THAT WILL BE USED IN TOP RIGHT CORNER OF MUSIC TAB*/
@@ -12,7 +13,32 @@ let menu_icon=document.createElement("img");
     menu_icon.style.marginTop = "10px";
     menu_icon.style.cssFloat = "right";
 
-    menu_icon.onclick = function(){
+let gleaming_streets_logo = document.createElement('img');
+    gleaming_streets_logo.src= "img/gleamingstreets_white.png";
+    gleaming_streets_logo.style.width="100px";
+    gleaming_streets_logo.style.height="100px";
+    gleaming_streets_logo.style.display="inline-block";
+    gleaming_streets_logo.style.marginTop ="30px";
+
+let studio_logo_header = document.createElement('img');
+    studio_logo_header.src= "img/234_white.png";
+    studio_logo_header.style.width="100px";
+    studio_logo_header.style.height="100px";
+    studio_logo_header.style.display="inline-block";
+    studio_logo_header.style.marginTop ="10px";
+    studio_logo_header.style.marginRight ="75px";
+    studio_logo_header.style.marginLeft ="75px";
+    //studio_logo_header.style.float="left";
+
+
+let rar_logo = document.createElement('img');
+    rar_logo.src= "img/rar_records_white.png";
+    rar_logo.style.width="150px";
+    rar_logo.style.height="100px";
+    rar_logo.style.display="inline-block";
+    rar_logo.style.marginTop ="10px";
+
+    menu_icon.onclick = function musicNav(){
 
         /***** REMOVES MENU ICON IMAGE ********/
         g_s.removeChild(menu_icon);
@@ -26,10 +52,11 @@ let menu_icon=document.createElement("img");
         menu_icon_white.style.marginRight="20px";
         menu_icon_white.style.marginTop="10px";
 
-
-
         let menu_icon_div = document.createElement("div");
         menu_icon_div.appendChild(menu_icon_white);
+        menu_icon_div.appendChild(gleaming_streets_logo);
+        menu_icon_div.appendChild(studio_logo_header);
+        menu_icon_div.appendChild(rar_logo);
         menu_icon_div.setAttribute('class','menu_icon_div');
         menu_icon_div.style.width = "100%";
         menu_icon_div.style.height = "150px";
@@ -39,14 +66,67 @@ let menu_icon=document.createElement("img");
         menu_icon_div.style.marginTop = "-220px";
         g_s.appendChild(menu_icon_div);
 
-        menu_icon_white.onclick=function(){
+        menu_icon_white.onclick=function removeMusicNav(){
             menu_icon_div.style.display="none";
             g_s.appendChild(menu_icon);
         }
+
+        gleaming_streets_logo.onclick= function changeGleamingStreets(){
+            let g_s_logo=document.createElement("img");
+            g_s_logo.src="img/gleamingstreets.png";
+            g_s_logo.style.height = "180px";
+            g_s_logo.style.width = "180px";
+            g_s_logo.style.marginTop = "15px";
+        
+        
+            g_s.innerHTML='';
+            g_s.style.background = "#EAD721";
+            g_s.style.height= 100+"%";
+            g_s.style.borderRadius= "20px";
+        
+            g_s.appendChild(menu_icon);
+            g_s.appendChild(g_s_logo);
+        }
+        studio_logo_header.onclick= function change234Studios(){
+            let studio_logo=document.createElement("img");
+            studio_logo.src="img/234.png";
+            studio_logo.style.height = "180px";
+            studio_logo.style.width = "180px";
+            studio_logo.style.marginTop = "15px";
+        
+        
+            studio.innerHTML='';
+            studio.style.background = "white";
+            studio.style.height= 100+"%";
+            studio.style.borderRadius= "20px";
+        
+            studio.appendChild(menu_icon);
+            studio.appendChild(studio_logo);
+        }
+
+        rar_logo.onclick=function changeRarRecords(){
+            let studio_logo=document.createElement("img");
+            studio_logo.src="img/rar_records.png";
+            studio_logo.style.height = "180px";
+            studio_logo.style.width = "250px";
+            studio_logo.style.marginTop = "15px";
+        
+        
+            studio.innerHTML='';
+            studio.style.background = "#C3073f";
+            studio.style.height= 100+"%";
+            studio.style.borderRadius= "20px";
+        
+            studio.appendChild(menu_icon);
+            studio.appendChild(studio_logo);
+        }
+
     }
 
 
-/*CREATION ENDED*/
+/************CREATION ENDED****************/
+
+/************* IMAGE SLIDESHOW ************/
 
 var i = image.length;
 
@@ -70,6 +150,8 @@ function previousImage(){
         slider_content.innerHTML = "<img src=img/about/"+image[i-1]+".jpg>";
 }
 
+/************* SLIDESHOW END ****************/
+
 function changeGleamingStreets(){
     let g_s_logo=document.createElement("img");
     g_s_logo.src="img/gleamingstreets.png";
@@ -85,6 +167,40 @@ function changeGleamingStreets(){
 
     g_s.appendChild(menu_icon);
     g_s.appendChild(g_s_logo);
+}
+
+function change234Studios(){
+    let studio_logo=document.createElement("img");
+    studio_logo.src="img/234.png";
+    studio_logo.style.height = "180px";
+    studio_logo.style.width = "180px";
+    studio_logo.style.marginTop = "15px";
+
+
+    studio.innerHTML='';
+    studio.style.background = "white";
+    studio.style.height= 100+"%";
+    studio.style.borderRadius= "20px";
+
+    studio.appendChild(menu_icon);
+    studio.appendChild(studio_logo);
+}
+
+function changeRarRecords(){
+    let studio_logo=document.createElement("img");
+    studio_logo.src="img/rar_records.png";
+    studio_logo.style.height = "180px";
+    studio_logo.style.width = "250px";
+    studio_logo.style.marginTop = "15px";
+
+
+    studio.innerHTML='';
+    studio.style.background = "#C3073f";
+    studio.style.height= 100+"%";
+    studio.style.borderRadius= "20px";
+
+    studio.appendChild(menu_icon);
+    studio.appendChild(studio_logo);
 }
 
 
